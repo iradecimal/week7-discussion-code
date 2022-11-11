e6dbbcd2e8c4767d97b0e5279aa6de2c87c0049d
iradecimal <iggesmundo@up.edu.ph>
2022-11-11 15:55:56 +0800

    end of discussion

--- lib/screens/todo_page.dart
+++ lib/screens/todo_page.dart
@@ -9,6 +9,8 @@ import 'package:provider/provider.dart';
 import 'package:week7_networking_discussion/models/todo_model.dart';
 import 'package:week7_networking_discussion/providers/todo_provider.dart';
 import 'package:week7_networking_discussion/screens/modal_todo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -21,77 +23,99 @@ class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     // access the list of todos in the provider
-    List<Todo> todoList = context.watch<TodoListProvider>().todo;
+    Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todos;
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo"),
       ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: ((context, index) {
-          final todo = todoList[index];
-          return Dismissible(
-            key: Key(todo.id.toString()),
-            onDismissed: (direction) {
-              // Delete item when swiped
-              context.read<TodoListProvider>().deleteTodo(todo.title);
-
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('${todo.title} dismissed')));
-            },
-            background: Container(
-              color: Colors.red,
-              child: const Icon(Icons.delete),
-            ),
-            child: ListTile(
-              title: Text(todo.title),
-              leading: Checkbox(
-                value: todo.completed,
-                onChanged: (bool? value) {
-                  context.read<TodoListProvider>().toggleStatus(index, value!);
-                },
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) => TodoModal(
-                          type: 'Edit',
-                          todoIndex: index,
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.create_outlined),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) => TodoModal(
-                          type: 'Delete',
-                          todoIndex: index,
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.delete_outlined),
-                  )
-                ],
-              ),
-            ),
+      body: StreamBuilder(
+      stream: todosStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Error encountered! ${snapshot.error}"),
           );
-        }),
-      ),
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData) {
+          return Center(
+            child: Text("No Todos Found"),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data?.docs.length,
+          itemBuilder: ((context, index) {
+            Todo todo = Todo.fromJson(
+                snapshot.data?.docs[index].data() as Map<String, dynamic>);
+            return Dismissible(
+              key: Key(todo.id.toString()),
+              onDismissed: (direction) {
+                context.read<TodoListProvider>().changeSelectedTodo(todo);
+                context.read<TodoListProvider>().deleteTodo();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('${todo.title} dismissed')));
+              },
+              background: Container(
+                color: Colors.red,
+                child: const Icon(Icons.delete),
+              ),
+              child: ListTile(
+                title: Text(todo.title),
+                leading: Checkbox(
+                  value: todo.completed,
+                  onChanged: (bool? value) {
+                    context
+                        .read<TodoListProvider>()
+                        .toggleStatus(index, value!);
+                  },
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // showDialog(
+                        //   context: context,
+                        //   builder: (BuildContext context) => TodoModal(
+                        //     type: 'Edit',
+                        //     todoIndex: index,
+                        //   ),
+                        // );
+                      },
+                      icon: const Icon(Icons.create_outlined),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        context.read<TodoListProvider>().changeSelectedTodo(todo);
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) => TodoModal(
+                            type: 'Delete',
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.delete_outlined),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+        );
+      },
+    ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
             context: context,
             builder: (BuildContext context) => TodoModal(
-              type: 'Add',
-              todoIndex: -1, // Flag to identify that this particular modal is for add
+              type: 'Add'
             ),
           );
         },
